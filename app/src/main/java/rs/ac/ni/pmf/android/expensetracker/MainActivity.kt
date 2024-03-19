5bca68071e5828a83bbc1913f6f50a3bc68faa9a
dsan22 <jankovicdusan00@gmail.com>
2024-03-19 21:16:17 +0100

    Added composable for adding more expenses

--- app/src/main/java/rs/ac/ni/pmf/android/expensetracker/MainActivity.kt
+++ app/src/main/java/rs/ac/ni/pmf/android/expensetracker/MainActivity.kt
@@ -3,13 +3,17 @@ package rs.ac.ni.pmf.android.expensetracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import rs.ac.ni.pmf.android.expensetracker.ui.AddExpense
 import rs.ac.ni.pmf.android.expensetracker.ui.ExpenseList
 import rs.ac.ni.pmf.android.expensetracker.ui.theme.ExpenseTrackerTheme
 
@@ -23,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ExpenseList()
+                    AddExpense(modifier = Modifier.fillMaxHeight().padding(5.dp))
                 }
             }
         }
---
+++ app/src/main/java/rs/ac/ni/pmf/android/expensetracker/ui/AddExpenseScrean.kt
@@ -0,0 +1,184 @@
+package rs.ac.ni.pmf.android.expensetracker.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import rs.ac.ni.pmf.android.expensetracker.model.Category
+import rs.ac.ni.pmf.android.expensetracker.model.getIconFromCategory
+import rs.ac.ni.pmf.android.expensetracker.R
+import java.lang.Exception
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddExpense(modifier:Modifier= Modifier){
+    var description by remember { mutableStateOf("") }
+    var expenseString by remember { mutableStateOf("") }
+    var expense by remember { mutableStateOf(0.0)}
+
+
+    var expanded by remember { mutableStateOf(false) }
+    val items: List<Category> by remember {mutableStateOf(Category.values().toList()) }
+    var selectedCategory: Category by remember {
+        mutableStateOf(items[0])
+    }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        TextField(
+           enabled = false,
+            value = "",
+            onValueChange = {  },
+            label = {
+                Row {
+                    Column( modifier= Modifier.height(30.dp) ,verticalArrangement = Arrangement.Center) {
+                        Text(text="Category: $selectedCategory", color = Color.Black)
+                    }
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Column(modifier= Modifier
+                        .width(30.dp)
+                        .padding(5.dp)) {
+                        Image( painter =  painterResource(id= getIconFromCategory(selectedCategory)),
+                            contentDescription =selectedCategory.toString() )
+                    }
+                    Spacer(modifier = Modifier.weight(1.0f)) // fill height with spacer
+                    Column(modifier= Modifier
+                        .width(30.dp)
+                        .padding(5.dp)
+                    ) {
+                        Image( painter =  painterResource(id= R.drawable.down_arrow ),
+                            contentDescription =selectedCategory.toString() )
+                    }
+
+                }
+
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp)
+                .clickable {
+                    expanded = true
+                },
+            colors =  TextFieldDefaults.textFieldColors(
+                disabledTextColor = Color.Black
+            )
+
+        )
+
+        Row {
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                items.forEach { item ->
+                    DropdownMenuItem(
+                        text = {
+                            Row {
+                                Column( modifier= Modifier.height(30.dp) ,verticalArrangement = Arrangement.Center) {
+                                    Text("Category: $item")
+                                }
+                                Spacer(modifier = Modifier.width(10.dp))
+                                Column(modifier= Modifier
+                                    .width(30.dp)
+                                    .padding(5.dp)) {
+                                    Image( painter =  painterResource(id= getIconFromCategory(item)),
+                                        contentDescription =item.toString() )
+                                }
+
+                            }
+                        },
+                        onClick = {
+                            selectedCategory=item
+                            expanded = false
+                            println(" $selectedCategory")
+                        })
+                }
+            }
+        }
+
+        TextField(
+            value = description,
+            onValueChange = { description = it },
+            label = { Text("Description") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp)
+        )
+        TextField(
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+            value = expenseString,
+            onValueChange = {
+                try {
+                    expense = it.toDouble()
+                    expenseString = it
+                }
+                catch (_:Exception){
+                }
+
+            },
+            label = { Text("Expense") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp)
+        )
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+
+            Button(modifier =Modifier.fillMaxWidth().padding(start =  20.dp,end=20.dp, top = 5.dp),onClick = { println("test") }) {
+                Text("Confirm")
+            }
+
+            Button(modifier =Modifier.fillMaxWidth().padding(start =  20.dp,end=20.dp,top = 5.dp) , onClick = { } ) {
+                Text("Back")
+            }
+
+        }
+    }
+
+
+}
+
+
+
+@Preview
+@Composable
+fun AddExpensePreview(){
+    AddExpense(
+        Modifier
+            .fillMaxHeight()
+            .padding(10.dp))
+}
+
+
